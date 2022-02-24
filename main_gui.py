5a22a17efbcdec3cd8adeb1373ae1642933e6ead
duncans007 <catking006@mgmail.com>
2022-02-24 13:55:06 -0500

    fixed error causing last guess to be skipped. Worldle class now needs generate_word to be called manually

--- main_gui.py
+++ main_gui.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from src.Worldle import Worldle
 
 
@@ -21,6 +21,7 @@ word_length = 5
 page_title = "Worldle"
 game_object = Worldle(word_length=word_length, max_guesses=max_guesses)
 guess_dict = reset_dict()
+error_msg = ""
 
 
 
@@ -29,58 +30,69 @@ app = Flask(__name__)
 
 
 ##### Flask ome game page
-@app.route("/")
-def worldle_game():
-    return render_template("./worldle_page.html", guesses=max_guesses, word_length=word_length, data=guess_dict, guesses_left=True, title=page_title, error_msg="")
+#@app.route("/")
+#def worldle_game():
+#    return render_template("./worldle_page.html", guesses=max_guesses, word_length=word_length, data=guess_dict, guesses_left=True, title=page_title, error_msg="")
 
 
 ##### Flask home page POST request
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['POST', 'GET'])
 def button_press():
+    global guess_dict, game_object, error_msg
 
-    global guess_dict, game_object
-    error_msg = ""
+    guesses_left = max_guesses >= game_object.guesses
 
-    # Handles guess making
-    if request.form['submit_button'] == "Make Guess":
+    if request.method == 'POST':
 
-        guesses_left = True
-        # get form input
-        text_guess = request.form['worldle_guess'].lower()
-        # pass guess to game object
-        game_return = game_object.guess(text_guess)
+        error_msg = ""
 
+        # Handles guess making
+        if request.form['submit_button'] == "Make Guess":
 
-        # error value 0, 1, 4: successfully made guess
-        if game_return[0] in [0, 1, 4]:
-            # update output dictionary
-            guess_dict[game_object.guesses - 1] = {
-                'letters': [*text_guess],
-                'colors': game_object.output_to_colors(game_return[1])
-            }
-            if game_return[0] == 0:
-                error_msg = ""
-            elif game_return[0] == 1:
-                error_msg = f"That's the last guess! Your word was \'{game_return[2]}\'."
-                guesses_left = False
-            elif game_return[0] == 4:
-                error_msg = "You guessed right!"
-                guesses_left = False
+            guesses_left = True
+            # get form input
+            text_guess = request.form['worldle_guess'].lower()
+            # pass guess to game object
+            game_return = game_object.guess(text_guess)
 
 
-        # error value 2: guess not proper length
-        # error value 3: not an actual word
-        # error value 5: is not alpha
-        elif game_return[0] in [2,3,5]:
-            error_msg = game_return[1]
+            # error value 0, 1, 4: successfully made guess
+            if game_return[0] in [0, 1, 4]:
+                # update output dictionary
+                guess_dict[game_object.guesses - 1] = {
+                    'letters': [*text_guess],
+                    'colors': game_object.output_to_colors(game_return[1])
+                }
+                if game_return[0] == 0:
+                    error_msg = ""
+                elif game_return[0] == 1:
+                    error_msg = f"That's the last guess! Your word was \'{game_return[2]}\'."
+                    guesses_left = False
+                elif game_return[0] == 4:
+                    error_msg = "You guessed right!"
+                    guesses_left = False
 
 
-    # Handle reset button
-    elif request.form['submit_button'] == "Reset":
-        guess_dict = reset_dict()
-        game_object = Worldle(word_length=word_length, max_guesses=max_guesses)
-        guesses_left = True
+            # error value 2: guess not proper length
+            # error value 3: not an actual word
+            # error value 5: is not alpha
+            elif game_return[0] in [2,3,5]:
+                error_msg = game_return[1]
 
+
+        # Handle reset button
+        elif request.form['submit_button'] == "Reset":
+            guess_dict = reset_dict()
+            game_object = Worldle(word_length=word_length, max_guesses=max_guesses)
+
+
+        return redirect(url_for('button_press'))
+
+    try:
+        if error_msg:
+            pass
+    except UnboundLocalError:
+        error_msg = ""
 
     # return template
     return render_template("./worldle_page.html", guesses=max_guesses, word_length=word_length, data=guess_dict, guesses_left=guesses_left, title=page_title, error_msg=error_msg)
--- src/Worldle.py
+++ src/Worldle.py
@@ -32,59 +32,61 @@ class Worldle:
         self.word_solution = self.word_list[idx]
         #self.word_list.pop(idx)
         self.guesses = 1
+        self.taking_guesses = True
 
 
     ##### Function steps forward when a guess is taken by inputting to this function
     def guess(self, word_guess):
-        ##### Return codes
-        # 0: successfully made guess
-        # 1: last guess taken, show word
-        # 2: guess word not proper length
-        # 3: guess word not an actual word
-        # 4: guessed word correctly
-        # 5: is not alpha
+        if self.taking_guesses:
+            ##### Return codes
+            # 0: successfully made guess
+            # 1: last guess taken, show word
+            # 2: guess word not proper length
+            # 3: guess word not an actual word
+            # 4: guessed word correctly
+            # 5: is not alpha
 
-        ##### Check for error codes
-        # Make sure the word is letters only
-        if not word_guess.isalpha():
-            return [5, "Error 5: Word is not letters only. Please try again with letters only."]
+            ##### Check for error codes
+            # Make sure the word is letters only
+            if not word_guess.isalpha():
+                return [5, "Error 5: Word is not letters only. Please try again with letters only."]
 
-        # Make sure the word is the proper length
-        if len(word_guess) != self.word_length:
-            return [2, f"Error 2: Word is not proper length. Please try again with word length of {self.word_length}."]
+            # Make sure the word is the proper length
+            if len(word_guess) != self.word_length:
+                return [2, f"Error 2: Word is not proper length. Please try again with word length of {self.word_length}."]
 
-        # Check if word is in dictionary of valid words
-        if word_guess not in self.word_list:
-            return [3, "Error 3: Word is not in dictionary of valid words."]
+            # Check if word is in dictionary of valid words
+            if word_guess not in self.word_list:
+                return [3, "Error 3: Word is not in dictionary of valid words."]
 
 
-        ##### Check words for matching letters
-        matched_output = []
-        self.guesses = self.guesses + 1
-        for g, s in zip(word_guess, self.word_solution):
-            # Test for matching letters in proper place
-            if g == s:
-                matched_output.append(1)
-            # Test for matching letters in improper place
-            elif g in self.word_solution:
-                matched_output.append(2)
-            # If neither condition is satisfied, letter does not exist
+            ##### Check words for matching letters
+            matched_output = []
+            self.guesses = self.guesses + 1
+            for g, s in zip(word_guess, self.word_solution):
+                # Test for matching letters in proper place
+                if g == s:
+                    matched_output.append(1)
+                # Test for matching letters in improper place
+                elif g in self.word_solution:
+                    matched_output.append(2)
+                # If neither condition is satisfied, letter does not exist
+                else:
+                    matched_output.append(0)
+
+
+            ##### Check for perfectly matching words
+            if matched_output == [1]*self.word_length:
+                return [4, matched_output]
+
+
+            ##### Send solution if max guesses is reached
+            if self.guesses <= self.max_guesses:
+                return [0, matched_output]
             else:
-                matched_output.append(0)
-
-
-        ##### Check for perfectly matching words
-        if matched_output == [1]*self.word_length:
-            return [4, matched_output]
-
-
-        ##### Send solution if max guesses is reached
-        if self.guesses <= self.max_guesses:
-            return [0, matched_output]
-        else:
-            output_word = self.word_solution
-            self.generate_word()
-            return [1, matched_output, output_word]
+                output_word = self.word_solution
+                self.taking_guesses = False
+                return [1, matched_output, output_word]
 
 
     def output_to_colors(self, matched_outp, colors=['gray', 'green', 'yellow']):
