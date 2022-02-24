22823067b9c04b040a6346c881782fafb7e49209
duncans007 <catking006@mgmail.com>
2022-02-24 13:59:11 -0500

    updated cli to match new manual reset for worldle class

--- main_cli.py
+++ main_cli.py
@@ -1,12 +1,16 @@
 from src.Worldle import Worldle
 
 def main():
-    worldle = Worldle(max_guesses=6, word_length=4)
+    max_guesses = 6
+    worldle = Worldle(max_guesses=max_guesses, word_length=4)
     while True:
         print(f"Guess {worldle.guesses}: ", end="")
         inp = input()
         ret = worldle.guess(inp)
         print(ret)
+        if max_guesses < worldle.guesses:
+            worldle.generate_word()
+            print("\nNew Game!\n")
 
 
 if __name__ == "__main__":
